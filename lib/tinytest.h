90bb189e3306ff723040f65f7738ccf9474faee5
9takoyakii <ygysn21@gmail.com>
2025-06-17 22:37:12 +0800

    Update version: 75cd8c1544a235b2883412207b16e37d5e7ba892

--- lib/tinytest.h
+++ lib/tinytest.h
@@ -3,7 +3,7 @@
 //              for the C programming language.
 //
 // Repo: https://github.com/9takoyakii/tinytest-c
-// Version: f85fb267acc09c24856298929e5f93160bfd9178
+// Version: 75cd8c1544a235b2883412207b16e37d5e7ba892
 // SPDX-License-Identifier: WTFP
 //
 
